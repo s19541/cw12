956aca701ab7ba9ccca79bfd08e32299c67cbc8f
Marcin Wałachowski <s19541@pjwstk.edu.pl>
2020-05-25 11:44:23 +0200

    cw12 (przerobione do dodawania modelu)

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        /* public string Index()
+         {
+             return "This is my default action...";
+         }*/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        /* public string Welcome(string name, int numTimes=1)
+         {
+            // return "This is the Welcome action method...";
+             return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+         }*/
+        /*public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }*/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
